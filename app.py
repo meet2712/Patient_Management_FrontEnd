6e46eb39a8863fec1980425b1efcf564ea273b5e
Keval <keval.bda1716@ict.gnu.ac.in>
2021-03-11 12:57:55 +0530

    CSS error fixed

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/form_login', methods=['POST','GET'])
 def index():
+    return render_template('login.html')
+
+@app.route('/form_signup', methods=['POST','GET'])
+def signp():
     return render_template('signup.html')
 
 
