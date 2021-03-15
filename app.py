1fbde3ed0290141470fd08fff6f86b9b7d8d9974
Meet <meet.bda1729@ict.gnu.ac.in>
2021-03-15 22:20:19 +0530

    Login Done & Connected

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import *
 import requests
 
@@ -9,6 +10,11 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/test', )
+def test(value):
+    return render_template('test.html', value)
+
+
 @app.route('/login', methods=['POST','GET'])
 def login():
     return render_template('login.html')
@@ -16,18 +22,33 @@ def login():
 
 @app.route('/validate', methods=['POST'])
 def validate():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        url = "https://patient-managment-api.herokuapp.com/token"
 
-        payload = {'username': username,
-                   'password': password}
+    username = request.form.get("username")
+    password = request.form.get("password")
+    url = "https://patient-managment-api.herokuapp.com/token"
 
-        response = requests.request("POST", url, data=payload)
-        json_data = json.loads(response.text)
-        return jsonify(json_data)
-    return redirect(url_for("home"))
+    payload = {'username': username,
+               'password': password}
+
+    response = requests.request("POST", url, data=payload)
+    json_data = json.loads(response.text)
+    print(type(json_data))
+    return render_template('test.html', value=json_data)
+
+
+@app.route('/patient',  methods=['POST','GET'])
+def patient():
+    url = "https://patient-managment-api.herokuapp.com/patient"
+
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NCwibmFtZSI6Im1lZXQiLCJ1c2VybmFtZSI6Im1lZXQyNzEyIiwidXNlcnR5cGUiOiJhZG1pbiIsInBhc3N3b3JkX2hhc2giOiIkMmIkMTIkM0tkeldkcXpFQ3pOVTlGZGZaZXAzdUhyaFdqNS5Td2l2MU1odVFNaUxxUlpMUkdxRVoydUMifQ.OmmTbiqaHTcslkz2ch1RRy9IrVDYfeGWmP1cw8T0tjE'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(type(response.text))
+    return render_template('trial.html')
 
 
 
