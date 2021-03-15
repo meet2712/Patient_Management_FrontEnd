c8a058179aaa3106849297b7b6bf58db405f61e4
Meet <meet.bda1729@ict.gnu.ac.in>
2021-03-15 20:13:08 +0530

    Login Done & Connected

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import *
+import requests
 
 app = Flask(__name__)
 
@@ -7,8 +8,31 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/login')
-def index():
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        url = "https://patient-managment-api.herokuapp.com/token"
+
+        payload = {'username': username,
+                   'password': password}
+
+        response = requests.request("POST", url, data=payload)
+        json_data = json.loads(response.text)
+        return jsonify(json_data)
+    return redirect(url_for("home"))
+
+
+
+@app.route('/signup', methods=['POST','GET'])
+def signp():
     return render_template('signup.html')
 
 
